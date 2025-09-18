3f404cc4af670429de6fb9a4531ff9b2a08d9608
Kenny3D1 <info@findomatix.com>
2025-09-18 13:10:09 -0400

    feat: update celery_worker

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -3,10 +3,9 @@ import os
 from celery import Celery
 from .config import settings
 
-# Avoid importing your whole config at import-time
 REDIS_URL = settings.REDIS_URL
 
-celery = Celery("fraudpop", broker=REDIS_URL, backend=REDIS_URL, include=["app.workers.tasks"],)
+celery = Celery("fraudpop", broker=REDIS_URL, backend=REDIS_URL)
 
 celery.conf.update(
     task_serializer="json",
@@ -15,3 +14,6 @@ celery.conf.update(
     broker_connection_retry_on_startup=True,
     worker_prefetch_multiplier=1,
 )
+
+# If your tasks live under app/workers/ (with __init__.py present)
+celery.autodiscover_tasks(["app.workers"])
